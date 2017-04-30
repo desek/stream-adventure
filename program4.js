aa703472cb3ffeb89f617d5ad5659a146c5ded44
Daniel Grenemark <daniel@grenemark.se>
2017-04-30 15:53:20 +0200

    program4.js

---
+++ program4.js
@@ -0,0 +1,6 @@
+var through = require('through2');
+
+process.stdin.pipe(through(function (chunk, enc, callback) {
+    this.push(chunk.toString().toUpperCase());
+    callback();
+})).pipe(process.stdout);
